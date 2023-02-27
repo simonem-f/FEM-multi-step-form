9c765d0caa239e924beaaa2d688706540dcba425
sam <92265654+simonem-f@users.noreply.github.com>
2023-02-28 00:02:54 +0100

    add form controls and validation (step 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { PrimeNGConfig } from 'primeng/api';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,53 @@ export class AppComponent implements OnInit {
 
   public carouselIndex = 0;
   public btnDisabled = false;
-  constructor(private primengConfig: PrimeNGConfig) {}
+
+  // NG reactive forms
+  public infoForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phone: [
+      '',
+      [Validators.required, Validators.pattern(new RegExp('^\\+?\\d{9,11}$'))],
+    ],
+  });
+  public yearly = false;
+  public plan: 1 | 2 | 3 = 1;
+  //public addOns = [];
+
+  public availablePlans = [
+    { id: 1, label: 'Arcade', monthPrice: 9, yearPrice: 90 },
+    { id: 2, label: 'Advanced', monthPrice: 12, yearPrice: 120 },
+    { id: 3, label: 'Pro', monthPrice: 15, yearPrice: 150 },
+  ];
+  public availableAddOns = [
+    {
+      service: 'Online service',
+      description: 'Access to multiplayer games',
+      monthPrice: 1,
+      yearPrice: 10,
+    },
+    {
+      service: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      monthPrice: 2,
+      yearPrice: 20,
+    },
+    {
+      service: 'Customizable Profilee',
+      description: 'Custom theme on your profile',
+      monthPrice: 2,
+      yearPrice: 20,
+    },
+  ];
+
+  constructor(private primengConfig: PrimeNGConfig, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.primengConfig.ripple = true;
   }
 
+  // carousel controls -------------------
   onSlideStart(): void {
     this.btnDisabled = true;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserAnimationsModule, BrowserModule, ButtonModule, NgbModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    ButtonModule,
+    NgbModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
